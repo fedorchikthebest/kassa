a03d9f78f86b09b98a17bf28ba6a25d48a9f7aae
gimonchik <fedorgorlenko@gmail.com>
2023-09-15 15:11:02 +0300

    databases

---
+++ app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template('index.html')
+
+
+@app.route("/add", methods=['GET', 'POST'])
+def add():
+    if request.method == 'POST':
+        name = request.form.get('product-name')
+        price = request.form.get('price')
+        quantity = request.form.get('quantity')
+        print(name, price, quantity)
+        return render_template('add.html', messege='Данные успешно получены и обработаны')
+    return render_template('add.html', messege='')
+
+
+app.run('127.0.0.1', port=5222)
