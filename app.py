a59b9f092686377ef3d5b48feff9beca7f673065
gimonchik <fedorgorlenko@gmail.com>
2023-09-15 15:36:28 +0300

    bug fix

--- app.py
+++ app.py
@@ -1,12 +1,13 @@
 from flask import Flask, render_template, request
 from data.db_session import global_init
+from db_operations import *
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def index():
-    return render_template('index.html')
+    return render_template('index.html', puchases=get_purchases())
 
 
 @app.route("/add", methods=['GET', 'POST'])
@@ -15,10 +16,10 @@ def add():
         name = request.form.get('product-name')
         price = request.form.get('price')
         quantity = request.form.get('quantity')
-        print(name, price, quantity)
+        add_purchase(name, quantity, price)
         return render_template('add.html', messege='Данные успешно получены и обработаны')
     return render_template('add.html', messege='')
 
 
 global_init('./db/db.db')
-app.run('127.0.0.1', port=5222)
+app.run('127.0.0.1', port=5000)
